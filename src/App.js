8118b05685e744eed2f3799a372361e57066e6d1
ohersi1 <osmanhersi@hotmail.co.uk>
2021-05-20 21:46:12 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const [ count, setCount ] = useState(0);
 
+// this produces a random number between 0 and 255
+const randomNumber = () => {
+   return Math.floor(Math.random() * 255) 
+  }
+
+ // this produces a random color for the styling of the h2 count variable
+let randomColor = 'rgb(' + randomNumber() + "," + randomNumber() + "," + randomNumber() + ')';
+
+// this is used to call the function that updates the count every 1000 milliseconds in this case
+useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevState => prevState + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Time Elapsed</h1>
+      <h2 style={{color: randomColor}}>{count} Seconds</h2>
+    </div>
+  )
+}
+// this is a non-named export so you can import it using whatever name you want as long as the path is correct
 export default App;
